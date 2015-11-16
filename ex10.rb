f4329b2652a3a851de5b30c2ece89962bb8aa269
Madeline <mcelis13@users.noreply.github.com>
2015-11-15 23:57:31 -0500

    ex10.rb

---
+++ ex10.rb
@@ -0,0 +1,40 @@
+# Variable tabby_cat declared and assigned a string value.  The output will be displayed
+# in tabbed format symbolized with the "\t" at the start of the string.
+tabby_cat = "\tI'm tabbed in."
+
+# Variable persian_cat declared and assigned the string value below.
+# The output will be displayed on two separate lines as indicated by the "\n" symbol
+persian_cat = "I'm split\non a line."
+
+# Variable backlash_cat declared and assigned the string value below.
+# the output will be displayed with a backslash where the "\\" symbol is shown.
+backslash_cat = "I'm \\ a \\ cat."
+
+# Variable fat_cat assigned a string input of multiple lines indicated by the """'s
+# the output display will be tabbed for each item indicated by the "/t"
+# each item will be output in a new line (due to the last item being in the same line
+# as the previous item the "\n" symbol was used to display that item in a new line.)
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip \n\t* Grass
+"""
+# Print out the variable tabby_cat
+puts tabby_cat
+
+# Print out the variable persian_cat
+puts persian_cat
+
+# Print out the variable backslash_cat
+puts backlash_cat
+
+# Print out the variable fat_cat
+puts fat_cat
+
+#Tasks:
+#1.Write down the mistakes you made. (Before I ran the program I caught that I had
+# misspelled the variable backslash_cat)
+#2.Memorize the escape sequences (made program ex10_chg1.rb to complete task)
+#3.Make new escape sequence combinations to create more complex string format.
+
