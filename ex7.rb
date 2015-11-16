90f9d8ecf04f19092a9e9ca2bf0a2e4d4f7a2e68
Madeline <mcelis13@users.noreply.github.com>
2015-11-15 23:55:22 -0500

    ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,61 @@
+#Tasks to complete
+#1.Write a comment explaining what each line of code does above it.
+#2. Write down the mistakes you made after proof reading the program.
+
+#Prints out the string below identified by ""'s
+puts "Mary had a little lamb."
+
+#Prints out the string and inserts another string within the string via the #{} symbol
+puts "Its fleece was white as #{'snow'}."
+
+#Prints out the string below.
+puts "And everywhere that Mary went."
+
+#Prints out the string ten times.
+puts "." * 10 # what'd that do?
+
+# Variable end1 declared and assigned a string value of "C"
+end1 = "C"
+
+# Variable end2 declared and assigned a string value of "h"
+end2 = "h"
+
+# Variable end3 declared and assigned a string value of "e"
+end3 = "e"
+
+# Variable end4 declared and assigned a string value of "e"
+end4 = "e"
+
+# Variable end5 declared and assigned a string value of "s"
+end5 = "s"
+
+# Variable end6 declared and assigned a string value of "e"
+end6 = "e"
+
+# Variable end7 declared and assigned a string value of "B"
+end7 = "B"
+
+# Variable end8 declared and assigned a string value of "u"
+end8 = "u"
+
+# Variable end9 declared and assigned a string value of "r"
+end9 = "r"
+
+# Variable end10 declared and assigned a string value of "g"
+end10 = "g"
+
+# Variable end11 declared and assigned a string value of "e"
+end11 = "e"
+
+# Variable end12 declared and assigned a string value of "r"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# Uses the function print to print the output without starting a newline after
+# the output-- to start a new line you would need to insert "/n"
+print end1 +end2 + end3 + end4 + end5 + end6
+
+#Uses the function put to print the output and start a new line after the output
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# When you try to save the file however it does give you an error message the first time.
