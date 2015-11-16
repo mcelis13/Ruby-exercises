6e2efdedda465ea01cd6e6e5ee3da50791b0403e
Madeline <mcelis13@users.noreply.github.com>
2015-11-15 23:58:13 -0500

    ex11.rb

---
+++ ex11.rb
@@ -0,0 +1,15 @@
+print "How old are you?"
+age = gets.chomp
+print "How tall are you?"
+height = gets.chomp
+print "How much do you weigh?"
+weight = gets.chomp
+
+#(added the word years to make the sentence flow better when returned to the user.)
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+# Tasks
+# 1. Find out what gets.chomp does.
+# gets.chomp -- It takes the input of the user and displays it on the same line (suppresses a new line from being created)
+# 2. Create examples to show new ways to use gets.chomp
+# 3. Write another form that asks the user for input-- (ex11_newform.rb)
