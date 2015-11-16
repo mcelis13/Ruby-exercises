7a1da720c87f73e8057d7c1fac94cf3264fc94f6
Madeline <mcelis13@users.noreply.github.com>
2015-11-15 23:56:47 -0500

    ex9.rb

---
+++ ex9.rb
@@ -0,0 +1,26 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# Variable days declared and assigned the following string input.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# Variable months declared and assigned the folloing string input.
+# (I just went ahead and started the Jan output on a new line to keep all the months in a neat list.)
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print out the string and insert the variable days witin the string.
+puts "Here are the days: #{days}"
+
+# Print out the string and insert the variable months within.
+puts "Here are the months: #{months}"
+
+# Print out the following strings.  The three """ marks  at the beginning
+# allow for multiple strings to be input at the same time and by placing the
+# three """ marks at the end of the string you signal the end of the string input.
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+# Task: Write down your mistakes.
+# No mistakes were made in making this program.
